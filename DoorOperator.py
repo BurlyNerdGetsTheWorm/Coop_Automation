bd5ec90c85adb9226bd4366afdf042987dd1c7a7
BurlyNerdGetsTheWorm <gdotmarsh@gmail.com>
2018-10-30 17:42:32 -0400

    Cleaned up print messages to be helpful instead of a data dump

--- DoorOperator.py
+++ DoorOperator.py
@@ -8,18 +8,19 @@ with open('values.json', 'r') as values:
     OpenState = json_data['open']
     Enabled = json_data['enabled']
 
-print(OpenState)
+print"Door is currently",OpenState
+print "Enabled is",Enabled
 
 #Set Sunrise and set times
-
 response = requests.get("https://apps.tsa.dhs.gov/MyTSAWebService/GetEventInfo.ashx?eventtype=sunrise_sunset&airportcode=FNT&output=json")
 SunRiseSetData = response.json()
 
 SunriseTime = SunRiseSetData["Sunrise"]
 SunsetTime = SunRiseSetData["Sunset"]
 
-print(SunriseTime)
-print(SunsetTime)
+#print sunrise sunset
+print "Sunrise is",SunriseTime
+print "Sunset is",SunsetTime
 
 #Get Current time (no date)
 from datetime import datetime
@@ -27,28 +28,22 @@ Currenthour = datetime.now().time().strftime("%I")
 AMPM = datetime.now().time().strftime("%p")
 int_Currenthour = int(Currenthour)
 
-#print the current time
-print(datetime.now().time())
-
-print(int_Currenthour)
-print(AMPM)
-print(Enabled)
-print(int(SunriseTime[:2]))
-print(int(SunsetTime[:2])+1)
+#print time values
+print "Current time is",datetime.now().time()
+print "Current hour is between",int_Currenthour,AMPM,"and",int_Currenthour+1,AMPM
+print "Opening hour is between",int(SunriseTime[:2])+1,AMPM,"and",int(SunriseTime[:2])+2,AMPM
+print "Closing hour is between",int(SunsetTime[:2])+1,AMPM,"and",int(SunsetTime[:2])+2,AMPM
 
 #Is it after sunrise?
 if ((int_Currenthour == (int(SunriseTime[:2])+1)) and (AMPM == "AM") and (Enabled is True)):
 	print("Sunrise conditional is true")
 	exec(open("DoorOpen.py").read())
 else:
-	print("It is not currently sunrise")
-
-
-
+	print("It is not currently time to open")
 
 #Is it after Sunset?
 if ((int_Currenthour == (int(SunsetTime[:2])+1)) and (AMPM == "PM") and (Enabled is True)):
 	print("Sunset conditional is true")
 	exec(open("DoorClose.py").read())
 else:
-	print("It is not currently sunset")
+	print("It is not currently time to close")
